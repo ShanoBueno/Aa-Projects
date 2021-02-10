f912a74eb37c06c994732de2936e460bb7813d12
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-02-09 15:30:45 -1000

    Change to salary_search

--- W4D2/class_inheritance.rb
+++ W4D2/class_inheritance.rb
@@ -13,38 +13,18 @@ class Employee
     end
   end
 
-
-
-
-
-
   def bonus(multiplier)
-    if title != manager
-      bonus = salary * multiplier
+    if @title != 'Manager'
+      bonus = @salary * multiplier
     else
       bonus = self.salary_search * multiplier
     end
   end
-
-  def salary_search(worker)
-    salaries = 0
-    if self.title == employee
-      salaries += self.salary
-    end
-      self.employees.each do |employee|
-        salary_search(employee)
-      end
-      salaries
-  end
-
-
-
-
 end
 
 class Manager < Employee
 
-  attr_reader :employees
+  attr_reader :employees, :salary, :title, :name, :boss
   
   def initialize(name, title, salary, boss)
     super
@@ -55,18 +35,60 @@ class Manager < Employee
     self.employees << employee
   end
 
+  # def bfs(node)
+  #   queue = []
+  #   queue.push(node)
+  #   while(queue.size != 0)
+  #     n = queue.shift
+  #     puts n.value
+  #     n.children.each do |child|
+  #       queue.push(child)
+  #     end
+  #   end
+  # end
+  # bfs(root)
+
+  def salary_search
+    funding = 0
+
+    queue = []
+    queue.push(self)
+    while (queue.size != 0)
+      n = queue.shift
+      puts n
+      puts n.salary
+      puts n.title
+      if n.title != "Employee"
+        n.employees.each do |employee|
+          funding += employee.salary
+        queue.push(employee)
+        else
+          funding += n.salary
+      end
+    end
+    funding
+    end
+  end
+  # def salary_search(worker)
+  #   salaries = 0
+  #   if @title == 'Employee'
+  #     salaries += self.salary
+  #   end
+  #     self.employees.each do |employee|
+  #       salary_search(employee)
+  #     end
+  #     salaries
+  # end
 
 
 end
 
+ned = Manager.new('Ned', 'Manager', 1000000, nil)
+darren = Manager.new('Darren', 'Manager', 78000, ned)
+shawna = Employee.new('Shawna', 'Employee', 12000, darren)
+david = Employee.new('David', 'Employee', 10000, darren)
 
-# p john = Employee.new('John', 'employee', 15, 'Jeff')
 
-# p penelope = Employee.new('Penelope', 'employee', 15, 'Jeff')
+puts shawna.bonus(2)
 
-p michael_scott = Manager.new('Michael', 'manager', 400, nil)
-
-p dwight = Manager.new('Dwight', 'manager', 10, michael_scott)
-
-p michael_scott.add_employee(dwight)
-p michael_scott.employees
+puts ned.bonus(5)
