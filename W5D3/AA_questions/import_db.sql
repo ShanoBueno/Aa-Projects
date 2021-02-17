208595791b988ea9d7c6d83fd4445c7fed488710
Shane <shane@Shanes-iMac.Home>
2021-02-17 17:50:09 -0500

    Creates Questions Tables

--- W5D3/AA_questions/import_db.sql
+++ W5D3/AA_questions/import_db.sql
@@ -1,21 +1,67 @@
+
+
+DROP TABLE if EXISTS question_likes;
+DROP TABLE if EXISTS replies;
+DROP TABLE if EXISTS question_follows;
+DROP TABLE if EXISTS questions;
+DROP TABLE if EXISTS users;
+
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
-  fname TEXT,
-  lname TEXT
-)
+  id INTEGER PRIMARY KEY, 
+  fname TEXT NOT NULL, 
+  lname TEXT NOT NULL 
+);
 
 CREATE TABLE questions (
-  title TEXT,
-  body TEXT,
-  author_id INTEGER,
-  FOREIGN KEY (author_id) REFERENCES (users.id)
-)
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL, 
+  body TEXT NOT NULL, 
+  author_id INTEGER NOT NULL, 
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
 
 CREATE TABLE question_follows (
-  author_id INTEGER,
-  question TEXT,
-  follower_id INTEGER,
-  FOREIGN KEY (question) REFERENCES (questions.title)
-  FOREIGN KEY (author_id) REFERENCES (users.id)
-  FOREIGN KEY (follower_id) REFERENCES (users.id)
-)
+  author_id INTEGER NOT NULL, 
+  question_id INTEGER NOT NULL, 
+  follower_id INTEGER NOT NULL, 
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (author_id) REFERENCES users(id),
+  FOREIGN KEY (follower_id) REFERENCES users(id)
+);
+
+CREATE TABLE replies(
+  id INTEGER PRIMARY KEY,
+  body TEXT NOT NULL,
+  question_id INTEGER NOT NULL,
+  author_id INTEGER NOT NULL,
+  parent_reply_id INTEGER,
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (author_id) REFERENCES users(id),
+  FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
+);
+
+
+CREATE TABLE question_likes (
+  author_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+INSERT INTO
+users(fname, lname)
+VALUES
+  ('Ned', 'Question'),
+  ('Kush', 'Question'),
+  ('Earl', 'Question');
+
+INSERT INTO 
+  questions(title,body,author_id)
+VALUES
+  ('Ned Question', 'Ned Ned Ned',1), 
+  ('Kush Question', 'Kush Kush Kush',2), 
+  ('Earl Question', 'Earl Earl Earl',3); 
+
+
