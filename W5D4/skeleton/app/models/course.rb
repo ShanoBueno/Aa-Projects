a5c4207c287501eeea380cdddccc3c92c8e5a5eb
Shane <shane@Shanes-iMac.Home>
2021-02-18 17:34:06 -0500

    Associates Enrollment Model

--- W5D4/skeleton/app/models/course.rb
+++ W5D4/skeleton/app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ApplicationRecord
+  has_many :enrollments,
+    foreign_key: :course_id,
+    class_name: :Enrollment
 end
--- W5D4/skeleton/app/models/user.rb
+++ W5D4/skeleton/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :enrollments,
+    foreign_key: :student_id,
+    class_name: :Enrollment
 end
