d3fbb5387dbe67cc117fdc50187e18731552de75
Chris Hanma <hongchris96@hotmail.com>
2021-02-17 17:02:35 -0800

    Creates database and ruby execution file

--- W5D3/AA_questions/import_db.sql
+++ W5D3/AA_questions/import_db.sql
@@ -22,7 +22,8 @@ CREATE TABLE questions (
   FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
-CREATE TABLE question_follows ( 
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY, 
   question_id INTEGER NOT NULL, 
   follower_id INTEGER NOT NULL, 
   FOREIGN KEY (question_id) REFERENCES questions(id),
@@ -42,6 +43,7 @@ CREATE TABLE replies(
 
 
 CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY, 
   like_user_id INTEGER NOT NULL,
   question_id INTEGER NOT NULL,
   FOREIGN KEY (question_id) REFERENCES questions(id),
--- W5D3/AA_questions/questions.rb
+++ W5D3/AA_questions/questions.rb
@@ -12,15 +12,21 @@ class QuestionsDatabase < SQLite3::Database
   end
 end
 
-class Questions 
-  attr_accessor :
-  def self.find_by_id
+class Question 
+  attr_accessor :id, :title, :body, :author_id
+  def self.find_by_id(id)
     questions = QuestionsDatabase.instance.execute(<<-SQL, id)
       SELECT * FROM questions WHERE id = ?
     SQL
-      return nil unless 
-        questions.length > 0 
-      end
+    return nil unless questions.length > 0
+    Question.new(questions.first)
+  end
+
+  def self.find_by_author_id(author_id)
+    questions = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+      SELECT * FROM questions WHERE author_id = ?
+    SQL
+    questions.map { |hsh| Question.new(hsh) }
   end
   
   def initialize(options)
@@ -28,10 +34,106 @@ class Questions
     @title = options['title']
     @body = options['body']
     @author_id = options['author_id']
+  end
+end
 
+class User
+  attr_accessor :id, :fname, :lname
+  def self.find_by_id(id)
+    users = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT * FROM users WHERE id = ?
+    SQL
+    return nil unless users.length > 0 
+    User.new(users.first)
   end
 
-
-
-
+  def self.find_by_name(fname, lname)
+    users = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+      SELECT * FROM users WHERE fname = ? AND lname = ?
+    SQL
+    users.map { |hsh| User.new(hsh) }
+  end
+  
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
 end
+
+class Reply
+  attr_accessor :id, :body, :question_id, :replier_id, :parent_reply_id
+  def self.find_by_id(id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT * FROM replies WHERE id = ?
+    SQL
+    return nil unless replies.length > 0 
+    Reply.new(replies.first)
+  end
+  
+  def self.find_by_replier_id(replier_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, replier_id)
+      SELECT * FROM replies WHERE replier_id = ?
+    SQL
+    replies.map { |hsh| Reply.new(hsh) }
+  end
+
+  def self.find_by_question_id(question_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT * FROM replies WHERE question_id = ?
+    SQL
+    replies.map { |hsh| Reply.new(hsh) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @body = options['body']
+    @question_id = options['question_id']
+    @replier_id = options['replier_id']
+    @parent_reply_id = options['parent_reply_id']
+  end
+end
+
+class QuestionFollow
+  attr_accessor :id, :question_id, :follower_id
+  def self.find_by_id(id)
+    question_follows = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT * FROM question_follows WHERE id = ?
+    SQL
+    return nil unless question_follows.length > 0 
+    QuestionFollow.new(question_follows.first)
+  end
+  
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @follower_id = options['follower_id']
+  end
+end
+
+class QuestionLike
+  attr_accessor :id, :question_id, :like_user_id
+  def self.find_by_id(id)
+    question_likes = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT * FROM question_likes WHERE id = ?
+    SQL
+    return nil unless question_likes.length > 0 
+    QuestionLike.new(question_likes.first)
+  end
+  
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @like_user_id = options['like_user_id']
+  end
+end
+
+# p User.find_by_name("Kush", "Push")
+# p User.find_by_id(2)
+# p Question.find_by_id(1)
+# p Question.find_by_author_id(1)
+# p Reply.find_by_id(2)
+# p Reply.find_by_replier_id(2)
+# p Reply.find_by_question_id(2)
+# p QuestionFollow.find_by_id(2)
+# p QuestionLike.find_by_id(2)
