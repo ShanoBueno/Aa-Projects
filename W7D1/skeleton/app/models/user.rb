9fed0d5c935ae423748a618e35f4242e81991b05
jhnegbrt <jhnegbrt@gmail.com>
2021-03-01 15:09:42 -0800

    Adds sessions view

--- W7D1/skeleton/app/models/user.rb
+++ W7D1/skeleton/app/models/user.rb
@@ -4,28 +4,43 @@ class User < ApplicationRecord
 
  after_initialize :ensure_session_token
 
+ def self.generate_session_token
+  SecureRandom::urlsafe_base64(16)
+ end
+
+ def self.find_by_credentials(user_name, password)
+
+  user = User.find_by(username: username)
+  return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+  nil
+
+ end
+
  def reset_session_token!
   self.session_token = User.generate_session_token
   self.save!
   self.session_token
  end
 
- def self.generate_session_token
-  SecureRandom::urlsafe_base64(16)
- end
+
 
  def password=(password)
   @password = password
   self.password_digest = BCrypt::Password.create(password)
  end
-end
 
 
+ def is_the_password?
 
+  BCrypt::Password.new(self.password_digest).is_password?(password)
+
+ end
 
  def ensure_session_token
   self.session_token ||= User.generate_session_token
  end
+
+
 
 
 end
