b32b7aa4a54448d972cdcb1d98431515595846fe
jhnegbrt <jhnegbrt@gmail.com>
2021-03-01 16:41:08 -0800

    finishes destroy method

--- W7D1/skeleton/app/controllers/sessions_controller.rb
+++ W7D1/skeleton/app/controllers/sessions_controller.rb
@@ -6,4 +6,25 @@ class SessionsController < ApplicationController
 
   end
 
+  def create
+
+    user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
+
+    session[:session_token] = user.reset_session_token!
+
+    redirect_to cats_url
+
+    # if user
+
+  end
+
+  def destroy
+
+    if current_user 
+      current_user.reset_session_token!
+      session[:session_token] = nil 
+    end
+
+  end
+
 end
